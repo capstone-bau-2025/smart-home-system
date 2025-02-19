446e9ba21c1169aa8fdee97a1258fad78a7256ba
Hamza Elzarw <hamzaelzarw@gmail.com>
2025-02-19 18:25:56 +0300

    Create centralhub Spring Boot project

---
+++ centralhub/src/main/java/com/capstonebau2025/centralhub/CentralhubApplication.java
@@ -0,0 +1,13 @@
+package com.capstonebau2025.centralhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralhubApplication.class, args);
+	}
+
+}
---
+++ centralhub/src/test/java/com/capstonebau2025/centralhub/CentralhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstonebau2025.centralhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
