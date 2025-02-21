deb782963d1a6c750baa0b508388d12ba4aaf5e2
Khaled AlAkkad <khaledakkad2003@gmail.com>
2025-02-21 16:19:37 +0300

    initial commit

---
+++ mobileapp/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Smart Home!!!</Text>
+      <Text>Welcome!</Text>
+      <Text>Test</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
