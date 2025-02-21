424a3f9e5b38d476beaba9dfe1a4c8f5c344c26b
Abdulrauf-Laydi <abdalruof.khl@gmail.com>
2025-02-21 16:37:08 +0300

    initialize cloudserver project

---
+++ cloudserver/src/main/java/com/capstonebau2025/cloudserver/CloudserverApplication.java
@@ -0,0 +1,13 @@
+package com.capstonebau2025.cloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudserverApplication.class, args);
+	}
+
+}
---
+++ cloudserver/src/test/java/com/capstonebau2025/cloudserver/CloudserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstonebau2025.cloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
