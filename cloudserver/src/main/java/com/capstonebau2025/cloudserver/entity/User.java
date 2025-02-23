be584ee01dea38849268f5ad36d9d74defc89340
Hamza Elzarw <hamzaelzarw@gmail.com>
2025-02-23 22:03:48 +0300

    Implement basic spring security setup for cloud server with JWT

---
+++ cloudserver/src/main/java/com/capstonebau2025/cloudserver/entity/User.java
@@ -0,0 +1,60 @@
+package com.capstonebau2025.cloudserver.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // Use email as the username
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // Account never expires
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // Account is never locked
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // Credentials never expire
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // Account is always enabled
+    }
+}
