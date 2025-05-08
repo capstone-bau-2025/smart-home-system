1fe801846271501ae3bad7917e5b2fd4295ac6d7
Hamza Elzarw <hamzaelzarw@gmail.com>
2025-05-08 22:46:30 +0300

    prepared docker containerization for central hub & refactored for cloud

--- centralhub/src/main/java/com/capstonebau2025/centralhub/config/AppConfig.java
+++ centralhub/src/main/java/com/capstonebau2025/centralhub/config/AppConfig.java
@@ -1,15 +1,10 @@
 package com.capstonebau2025.centralhub.config;
 
 import com.capstonebau2025.centralhub.client.WebsocketClient;
-import com.capstonebau2025.centralhub.client.CloudClient;
-import com.capstonebau2025.centralhub.dto.cloudComm.HubRegistrationResponse;
-import com.capstonebau2025.centralhub.entity.Hub;
-import com.capstonebau2025.centralhub.service.HubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -21,20 +16,6 @@ public class AppConfig {
     }
 
     @Bean
-    @Order(1)
-    public CommandLineRunner registerHubToCloud(CloudClient cloudClient, HubService hubService) {
-        return args -> {
-            Hub hub = hubService.initializeHubIfNeeded();
-
-            if(hub.getKey() == null) {
-                HubRegistrationResponse response = cloudClient.registerHub(hub);
-                hubService.setHubKey(response.getKey());
-            }
-        };
-    }
-
-    @Bean
-    @Order(2)
     public CommandLineRunner connectToCloudServer(WebsocketClient websocketClient) {
         return args -> {
             websocketClient.connectToCloud();
