206851e6e753dd4873bead3f4df29cf69387c3d8
Hamza Elzarw <hamzaelzarw@gmail.com>
2025-05-22 17:00:34 +0300

    validate device is not used in automation on deletion

--- centralhub/src/main/java/com/capstonebau2025/centralhub/repository/AutomationTriggerRepository.java
+++ centralhub/src/main/java/com/capstonebau2025/centralhub/repository/AutomationTriggerRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface AutomationTriggerRepository extends JpaRepository<AutomationTrigger, Long> {
     Optional<AutomationTrigger> findByAutomationRuleId(Long automationRuleId);
 
+    boolean existsByDeviceId(Long deviceId);
 }
